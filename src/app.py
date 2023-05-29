40ff4b359e2b4f62b6e180ee3741d8c73335e0bc
tanaji-chavan <tanajic@cybage.com>
2023-05-29 11:01:16 +0530

    Updated Metrics

--- src/app.py
+++ src/app.py
@@ -7,24 +7,12 @@ from prometheus_client import make_wsgi_app
 from prometheus_client import Counter
 from appmetrics import metrics
 from prometheus_client import Summary
+from prometheus_client import Gauge
 
-                                     
-# App name and URL
-app_name = 'My Python App'
-app_url = 'http://localhost:5000'
-app = Flask('My Python App')
-# PyActuator endpoint URL and registration URL
-pyactuator_endpoint_url = 'http://localhost:5000/'
-registration_url = 'http://localhost:8000/register'
-request_counter = 0
-# Initialize PyActuator
-# pyactuator = Pyctuator(app, app_name, app_url, pyactuator_endpoint_url, registration_url)
 
-# #initialize pyactuator with flask app
-# pyactuator.init_app(app)
+app_name = 'Python Sample App'
+app = Flask('Python Sample App')
 
-# Enable logging
-# pyactuator.enable_logging()
 logger = logging.getLogger(app_name)
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s; %(name)s - %(levelname)s - %(message)s')
@@ -37,16 +25,14 @@ app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
 })
 s = Summary('request_latency_seconds', 'Description of summary')
 c = Counter('my_requests_total', 'HTTP Failures', ['method', 'endpoint'])
+error_counter = Counter('my_failures', 'No of failures')
+g = Gauge('my_inprogress_requests', 'Description of gauge')
 
-# Enable metrics
-metrics.new_histogram("test-histogram")
-metrics.new_gauge("test-counter")
-
-# pyactuator.enable_metrics()
 @app.before_request
 def logging_before():
     # Store the start time for the request
     app_ctx.start_time = time.perf_counter()
+    g.set_to_current_time() 
 
 @app.after_request
 def logging_after(response):
@@ -54,38 +40,81 @@ def logging_after(response):
     total_time = time.perf_counter() - app_ctx.start_time
     time_in_ms = int(total_time * 1000)
     # Log the time taken for the endpoint 
-    current_app.logger.info('%s ms %s %s %s', time_in_ms, request.method, request.path, dict(request.args))
+    logger.info('Prcessing Time : %s ms %s %s %s', time_in_ms, request.method, request.path, dict(request.args))
     return response
 
 @app.route('/test', methods=['GET'])
 def example():
-    # increment the request counter
-    global request_counter
-    request_counter += 1
-    metrics.metric("test-counter").notify(request_counter)
 
-    # pyactuator.increment_counter('api.example.calls')
     time.sleep(random.randint(0,2))
     # Log a message using Python logging
     if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
         client_ip = request.environ['REMOTE_ADDR']
+        logger.info(f'Received API request from client {client_ip}')
     else:
         client_ip = request.environ['HTTP_X_FORWARDED_FOR']
-
-    logger.info(f'Received API request from client {client_ip}')
+        logger.info(f'Received API request from client {client_ip}')
 
     # Custom Metrics
-
-
     s.observe(1)    # Observe 4.7 (seconds in this case)
     c.labels('get', '/test').inc()
-    c.labels('post', '/submit').inc()
+
     # Business logic
     data = {
         'message': 'This is sample application to use for Logs and Metrics'
     }
     logger.info(f'Received API request{data}')
     return jsonify(data)
+
+@app.route('/error', methods=['GET'])
+def error():
+
+    # Custom Metrics
+    s.observe(1)    # Observe 4.7 (seconds in this case)
+    c.labels('get', '/error').inc()
+    # Business logic
+    data = {
+        'message': 'This is Error Page displayed after error'
+    }
+    error_counter.inc()
+    logger.info(f'Received API request{data}')
+    return jsonify(data)
+
+# write code to handle 404 errors 
+@app.errorhandler(404)
+def not_found(e):
+    # note that we set the 404 status explicitly
+    c.labels('get', '/error').inc()
+    error_counter.inc()
+    logger.error(f'Error  {e}')
+    return jsonify(error=str(e)), 404
+
+# write code to handle 500 errors
+@app.errorhandler(500)
+def internal_error(e):
+    # note that we set the 500 status explicitly
+    c.labels('get', '/error').inc()
+    error_counter.inc()
+    logger.error(f'Error  {e}')
+    return jsonify(error=str(e)), 500
+
+# write code to handle 503 errors
+@app.errorhandler(503)
+def service_unavailable(e):
+    # note that we set the 503 status explicitly
+    c.labels('get', '/error').inc()
+    error_counter.inc()
+    logger.error(f'Error  {e}')
+    return jsonify(error=str(e)), 503
+
+# write code to handle any generic errors 
+@app.errorhandler(Exception)
+def unhandled_exception(e):
+    # note that we set the 500 status explicitly
+    c.labels('get', '/error').inc()
+    error_counter.inc()
+    logger.error(f'Error  {e}')
+    return jsonify(error=str(e)), 500
 
 
 from appmetrics.wsgi import AppMetricsMiddleware
