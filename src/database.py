b76893e0c758f2fb2f5d873b71404c1546cd3dcb
tanaji-chavan <tanajic@cybage.com>
2023-06-09 10:00:03 +0530

    Added DB details to Github Secrets

--- src/database.py
+++ src/database.py
@@ -1,4 +1,11 @@
+import sqlalchemy
+import os
 from flask_sqlalchemy import SQLAlchemy
 
-db_url = 'postgresql://ctgdevops:devops123@54.163.126.233:5432/ctg-devops-ktracker'
+db_user = os.environ.get('DB_USER_NAME')
+db_password = os.environ.get('DB_PASSWORD')
+db_name = os.environ.get('DB_NAME')
+db_host = os.environ.get('DB_HOST')
+
+db_url = f"postgresql://{db_user}:{db_password}@{db_host}:5432/{db_name}"
 db = SQLAlchemy()
