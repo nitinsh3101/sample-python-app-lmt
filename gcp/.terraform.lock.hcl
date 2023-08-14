324e7063d89f5b212ab37b619738ce5a96ff1f0f
tanaji-chavan <tanajic@cybage.com>
2023-08-14 17:42:05 +0530

    GCP Changes

--- gcp/.terraform.lock.hcl
+++ gcp/.terraform.lock.hcl
@@ -2,21 +2,21 @@
 # Manual edits may be lost in future updates.
 
 provider "registry.terraform.io/hashicorp/google" {
-  version     = "3.5.0"
-  constraints = "3.5.0"
+  version     = "4.77.0"
+  constraints = "4.77.0"
   hashes = [
-    "h1:seOBe8JM4Wzh44i0fqhrKpv9P3pP1lyulZJvTkyLD5w=",
-    "zh:1bd907d58a05ec25d48294f6a0d7ab215d7243daa8a100ee983981ca641d2ce9",
-    "zh:334c0ac7599da444434d5cd1045171b8decf4eec1d01a69dd9a343e62ca8ffc0",
-    "zh:738c0808e29d14e9508f121c1966fa557b3db83fb5b0ce9b32ffebfbcc610dbe",
-    "zh:8b4c910d558d3176f96c50bca9c3484edd0df2a7429bf1609cd5aca2cf1aeab8",
-    "zh:a1e3d0057158267c2d86a5fc46cfa50e86cf70a60f242a96c66710c48d97bc04",
-    "zh:a718d621c671cb6d1d951e070ed9f52710a2441fa14e1aee7c00e14396142f9a",
-    "zh:ac1381f6e42de469a2842af8ba110787c9a7c1a1b250a53fe0831b70f54a56f6",
-    "zh:af979d49936061c0c37c1df7ddfbf1bca6b1f347562c8917b4e063ac7c354af7",
-    "zh:af9a7e94706d4155f88b2547620edc2ebfeaf2c4bb8f670a75e04ab7470c6640",
-    "zh:d820fb164d3f7da15aa4963a59f05134362b28c983d0cfc30ee3e325bc3362cb",
-    "zh:e1cd06c6ed45dd0d644b4afec7b8499e7be6337d45f4b802c933dd0016efc326",
-    "zh:fe6e7ea5f1a1d7876ac9939eb8cfc1376536d25ad7ecc86f5d6660c5b09a9de3",
+    "h1:rPryyySJPfBmoG3OkG2czu8wrnO+Lw4qmf/nDU9UeKA=",
+    "zh:0f5d68c6166d4be73a3cd932ddc58324589db6557ca0d95462e21d3be9e5453e",
+    "zh:1e5cd136603ae54fb64dc42694ddee31b8a435f3b1c63d31601791ae771c6037",
+    "zh:21dea4fbf65e560414d7cf8ffea795ba90a5a4056efbde5035068465ff983d3c",
+    "zh:29070054dd61e5edefc9da92142d06d0a6d9799cee9076c90f8b85773979326d",
+    "zh:2fbe52ca8d9a4e47704fbec5ad781c42fa31867053fb409e292ec29e3b8b213a",
+    "zh:44276ddbb188d176e266bc4f0dbfcdb296efeaf038baa055480d86e7348fc420",
+    "zh:62c3b947f1dd4d213c2ea82ff4b11e92b34ce48149b35143504810dc719ef868",
+    "zh:7203adaa431207b6c67b617b94d37dfc499023ae0d5c56138151c9e30fe5666b",
+    "zh:7b482babc4df98b5ab496962ab5d7432be132207fd82ba9c6d35b42c7d7100a5",
+    "zh:c2e842b07c8e2b7623e18eaa0dfdcc2e786bf1883eb9dee2ada2347d47bce68c",
+    "zh:de18790b4ecba4e28618e319f34a2a13c52e376bec390dc94c3fdf53b71573f0",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
   ]
 }
--- gcp/main.tf
+++ gcp/main.tf
@@ -2,8 +2,8 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
-      version = "3.5.0"
+      source  = "hashicorp/google"
+      version = "4.77.0"
     }
   }
 }
@@ -14,60 +14,71 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "sample-python-app-lmt-network"
-}
-
-resource "google_compute_firewall" "firewall" {
-  name    = "sample-python-app-lmt-firewall"
-  network = google_compute_network.vpc_network.name
-
-  allow {
-    protocol = "icmp"
-  }
-
-  allow {
-    protocol = "tcp"
-    ports    = ["22", "80", "8080"]
-  }
-
-  source_ranges = ["103.81.78.0/24"]
-}
-
 # deploy sample-python-app-lmt to cloud run service 
 # Path: sample-python-app-lmt\sample-python-app-lmt\gcp\main.tf
 
-resource "google_cloudbuild_trigger" "sample-python-app-lmt" {
-  trigger_template {
-    branch_name = "main"
-    repo_name   = "sample-python-app-lmt"
+resource "google_cloudbuild_trigger" "sample-python-app-lmt-trigger" {
+  source_to_build {
+    uri       = "https://github.com/cybage-devops/sample-python-app-lmt.git"
+    ref       = "refs/heads/main"
+    repo_type = "GITHUB"
   }
 
-  filename = "cloudbuild.yaml"
-  name     = "sample-python-app-lmt"
+  git_file_source {
+    path      = "cloudbuild.yaml"
+    uri       = "https://github.com/cybage-devops/sample-python-app-lmt.git"
+    revision  = "refs/heads/main"
+    repo_type = "GITHUB"
+  }
+
+  name = "sample-python-app-lmt-trigger"
 }
 resource "google_cloud_run_service" "sample-python-app-lmt" {
   name     = "sample-python-app-lmt"
   location = "us-central1"
-
+  # ingress = "INGRESS_TRAFFIC_ALL"
   template {
     spec {
       containers {
-        image = "us-central1-docker.pkg.dev/sample-python-app-lmt:latest"
+        # name = "sample-python-app-lmt"
+        image = "us-central1-docker.pkg.dev/cybage-devops/sample-python-app-lmt/sample-python-app-lmt:latest"
+        ports {
+          container_port = 5000
+        }
+        env {
+          name  = "DB_USER_NAME"
+          value = "ctgdevops"
+        }
+        env {
+          name  = "DB_HOST"
+          value = "40.76.150.114"
+        }
+        env {
+          name  = "DB_NAME"
+          value = "ctg_devops_ktracker"
+        }
+        env {
+          name = "DB_PASSWORD"
+          value_from {
+            secret_key_ref {
+              name = "cloud-run-python-app-db-secret"
+              key  = "1"
+            }
+          }
+        }
       }
     }
   }
-
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
-
+    traffic {
+      percent         = 100
+      latest_revision = true
+    }
+  
 }
 
-resource "google_cloud_run_service_iam_member" "sample-python-app-lmt" {
-  service = google_cloud_run_service.sample-python-app-lmt.name
+resource "google_cloud_run_service_iam_member" "sample-python-app-lmt-i-am" {
+  service  = google_cloud_run_service.sample-python-app-lmt.name
   location = google_cloud_run_service.sample-python-app-lmt.location
-  role = "roles/run.invoker"
-  member = "allUsers"
+  role     = "roles/run.invoker"
+  member   = "allUsers"
 }
